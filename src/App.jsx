f8313e8a24722283159f41516e369e394424db15
Jonathan Kullman <jontkulljobb@gmail.com>
2024-03-11 16:46:32 +0100

    feat: added meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./index.css";
-import styles from "./style";
 import { Hero } from "./components";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
+      <Helmet>
+        <title>Apaztron | Official Website</title>
+        <meta
+          name="description"
+          content="Official Website Of DJ and Producer Apaztron."
+        />
+        <meta name="keywords" content="Apaztron, Music, Hardstyle, DJ"></meta>
+      </Helmet>
       <div className="bg-primary">
         <div>
           <Hero />
